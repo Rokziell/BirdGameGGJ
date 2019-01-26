b93b5f4914c08c5947b0c83c2a4bdce61d2c95d7
Renato <renma2497@gmail.com>
2019-01-26 04:55:56 -0500

    Wind Force, Mixing Mechanics

--- Uccellino/Assets/RA Scripts/PlayerController.cs
+++ Uccellino/Assets/RA Scripts/PlayerController.cs
@@ -3,34 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
 
-    private Rigidbody rb;
-    public float speed = 10f;
+{
+    [SerializeField]
+    float speed, maxSpeed, minSpeed, acceleration, jumpSpeed, slowSpeed;
+
+    Vector3 forward, rigth;
+    Rigidbody rigid;
+
+    public int count = 0;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rigid = GetComponent<Rigidbody>();
+        speed = 0f;
+        maxSpeed = 7f;
+        minSpeed = 0f;
+        acceleration = 0.2f;
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        rigth = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
     }
 
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (Input.anyKey)
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                Move();
+            }
 
-    rb.AddForce(movement * speed);
+            if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
+            {
+                Decelerate();
+            }
 
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Flower") {
-            other.gameObject.SetActive(false);
+            if (Input.GetButtonDown("Jump") && transform.position.y < 0.6)
+            {
+                rigid.AddForce(Vector3.up * jumpSpeed * 100);
+            }
         }
     }
 
+    void Move()
+    {
+
+        if (speed <= maxSpeed)
+        {
+            speed += acceleration;
+        }
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 rigthMovement = rigth * speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        Vector3 heading = Vector3.Normalize(rigthMovement + upMovement);
+
+        //transform.forward = heading; //rotation happens
+        transform.position += rigthMovement; // movement happens
+        transform.position += upMovement; // movement happens
+    }
+
+    void Decelerate()
+    {
+        while (speed >= minSpeed)
+        {
+            speed -= acceleration;
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                break;
+            }
+        }
+        speed = Mathf.Clamp(speed, 0f, maxSpeed);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Flower")
+        {
+            if (Input.GetKeyDown("return"))
+            {
+
+                other.gameObject.SetActive(false);
+                count++;
+                maxSpeed -= slowSpeed;
+            }
+        }
+    }
 }
