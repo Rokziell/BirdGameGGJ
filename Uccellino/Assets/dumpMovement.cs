9a21eb24de5f6b9488d55d17ad94e9157258ac7a
orlando <orlax22@gmail.com>
2019-01-26 01:22:16 -0500

    started work on art, character now idles and moves

---
+++ Uccellino/Assets/dumpMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dumpMovement : MonoBehaviour
+{
+
+    public float speed = 4f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed*Time.deltaTime, transform.position.y, transform.position.z); 
+        Debug.Log(transform.position);
+    }
+}
