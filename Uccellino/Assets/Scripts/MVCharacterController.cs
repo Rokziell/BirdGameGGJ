78527911001d507bd5e25b4844c93ff5993bc39c
Maria De Vuono <medevuono@gmail.com>
2019-01-26 04:28:50 -0500

    locking movement view

---
+++ Uccellino/Assets/Scripts/MVCharacterController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MVCharacterController : MonoBehaviour
+{
+   // GameObject target;
+
+    [SerializeField]
+    float speed, maxSpeed, minSpeed, acceleration;
+
+    Vector3 forward, rigth;
+
+    void Start()
+    {
+        speed = 0f;
+        maxSpeed = 7f;
+        minSpeed = 0f;
+        acceleration = 0.2f;
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        rigth = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    void Update()
+    {
+
+        if (Input.anyKey)
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                Move();
+            }
+
+            if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
+            {
+               Decelerate();
+            }
+        }
+    }
+
+    void Move()
+    {
+
+        if (speed <= maxSpeed)
+        {
+            speed += acceleration;
+        }
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 rigthMovement = rigth * speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        Vector3 heading = Vector3.Normalize(rigthMovement + upMovement);
+
+        //transform.forward = heading; //rotation happens
+        transform.position += rigthMovement; // movement happens
+        transform.position += upMovement; // movement happens
+    }
+
+    void Decelerate()
+    {
+        while (speed >= minSpeed)
+        {
+            speed -= acceleration;
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                break;
+            }
+        }
+        speed = Mathf.Clamp(speed, 0f, maxSpeed);
+    }
+}
