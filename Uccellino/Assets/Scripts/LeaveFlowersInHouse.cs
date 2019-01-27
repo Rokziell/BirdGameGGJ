6e97f9f5afa9978abec43e149f3b1c415d495f6d
Renato <renma2497@gmail.com>
2019-01-26 21:24:22 -0500

    ChangeHouseSprite and Animation to flowers

--- Uccellino/Assets/Scripts/LeaveFlowersInHouse.cs
+++ Uccellino/Assets/Scripts/LeaveFlowersInHouse.cs
@@ -5,42 +5,59 @@ using UnityEngine;
 public class LeaveFlowersInHouse : MonoBehaviour
 {
     PlayerController characterVariable;
+    public HomeSpriteChanger homeChanger;
     public int maxFlowersInHouse;
-    private int flowersInTheHouse;
-
+    private int flowersInTheHouse, flowersForNextLevel, rangeToNextLevel;
+    private float timeDelay;
 
     // Start is called before the first frame update
     void Start()
     {
         characterVariable = GetComponent<PlayerController>();
+        timeDelay = 0.5f;
+        flowersForNextLevel = 2;
+        rangeToNextLevel = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-
-
-    //delegate void GameOVer();
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "House")
+        if (other.tag == "House")
         {
-            for (int i = characterVariable.currentSlot -1; i >= 0; i--)
+            for (int i = characterVariable.currentSlot - 1; i >= 0; i--)
             {
                 Debug.Log(i);
-                characterVariable.slots[i].sprite = null; 
+                characterVariable.slots[i].sprite = null;
                 characterVariable.maxSpeed += characterVariable.slowSpeed;
                 characterVariable.jumpSpeed++;
                 flowersInTheHouse++;
             }
             characterVariable.currentSlot = 0;
-            if(flowersInTheHouse >= maxFlowersInHouse)
+            if (flowersForNextLevel >= maxFlowersInHouse)
             {
-                //GAME OVER
+                homeChanger.currentHomeSprite++;
+                homeChanger.spriteRenderer.sprite = homeChanger.homeSprites[homeChanger.currentHomeSprite];
+                Debug.Log("GAME OVER!!!!");
+                gameOverVariable();
+            }
+            else
+            {
+                if (flowersInTheHouse >= flowersForNextLevel)
+                {
+                    homeChanger.currentHomeSprite++;
+                    homeChanger.spriteRenderer.sprite = homeChanger.homeSprites[homeChanger.currentHomeSprite];
+                    flowersForNextLevel += rangeToNextLevel;
+
+                }
             }
         }
     }
+
+    delegate void GameOver();
+    GameOver gameOverVariable;
 }
